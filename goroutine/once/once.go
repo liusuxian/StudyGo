45d5d9cc68dcd5415a0b132357f53eaac560c0dc
Liusuxian <382185882@qq.com>
2021-04-04 18:57:49 +0800

    update

---
+++ goroutine/once/once.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func main() {
+    var once sync.Once
+    // 第一个初始化函数
+    f1 := func() {
+        fmt.Println("in f1")
+    }
+    once.Do(f1) // 打印出 in f1
+
+    // 第二个初始化函数
+    f2 := func() {
+        fmt.Println("in f2")
+    }
+    once.Do(f2) // 无输出
+}
