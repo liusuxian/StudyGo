0341ee35801e298fc326868127e5b5b96918c3b2
liusuxian <382185882@qq.com>
2021-06-21 22:45:09 +0800

    update

---
+++ goroutine/channel/channel4/channel.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+type Token struct{}
+
+func newWorker(id int, ch chan Token, nextCh chan Token) {
+    for {
+        token := <-ch         // 取得令牌
+        fmt.Println((id + 1)) // id从1开始
+        time.Sleep(time.Second)
+        nextCh <- token
+    }
+}
+
+func main() {
+    chs := []chan Token{make(chan Token), make(chan Token), make(chan Token), make(chan Token)}
+
+    // 创建4个worker
+    for i := 0; i < 4; i++ {
+        go newWorker(i, chs[i], chs[(i+1)%4])
+    }
+
+    // 首先把令牌交给第一个worker
+    chs[0] <- struct{}{}
+
+    select {}
+}
